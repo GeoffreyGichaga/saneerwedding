8f2f6c0817628a0930ab27846bfb8ccf2d79f8fa
Jeff <geofreygichaga@gmail.com>
2023-01-06 17:36:09 +0300

    Add Navbar

---
+++ src/Components/MainNav.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../Styling/Nav.css'
+
+
+
+
+const MainNav = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" id='mainbar'>
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className='navlinks' href="#">Welcome</Nav.Link>
+            <Nav.Link className='navlinks' href="#">Our Story</Nav.Link>
+            <Nav.Link className='navlinks' href="#">Event Details</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link className='navlinks' href="#">Explore</Nav.Link>
+            <Nav.Link className='navlinks' href="#">Transport</Nav.Link>
+            <Nav.Link className='navlinks' href="#">Questions</Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+
+  )
+}
+
+export default MainNav
---
+++ src/Pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import MainNav from '../Components/MainNav'
+import '../Styling/Home.css'
+import Container from 'react-bootstrap/Container'
+
+
+
+
+const Home = () => {
+  return (
+    <>
+    <Container fluid id='firstblock'>
+     <MainNav/>
+
+
+    </Container>
+    
+    </>
+  )
+}
+
+export default Home
