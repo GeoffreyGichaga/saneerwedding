5e717c779a7d376e384f1b720c5a653a89ca72d3
Jeff <geofreygichaga@gmail.com>
2023-01-07 22:32:33 +0300

    welcome section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -24,7 +24,11 @@ const Home = () => {
 
     {/* Welcome Section */}
     <Container>
-        Hello
+        <h2 className='welcome-text mt-4 text-center'>Welcome</h2>
+        <p className='text-center'>Dear family and friends, the time has come <br/> To <span className='spantext'> raise the roof </span>  and have some <span className='spantext'> fun </span><br/>
+            Feel it in your heart and feel it in your <span className='spantext'> soul </span> <br/> Let the music <span className='spantext'>take control </span> <br/>Everybody <span className='spantext'>sing </span>, everybody <span className='spantext'>dance </span> <br/>
+            Lose yourself in wild romance <br/> We’re going to <br/> <span className='spantext'> Party, Love, Fiesta, Forever </span> <br/> Join us in Los Cabos!
+        </p>
     </Container>
     
 
