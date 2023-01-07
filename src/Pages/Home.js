9fca1f171345d04c8e0755f70000c6ce2d4bfd83
Jeff <geofreygichaga@gmail.com>
2023-01-07 22:53:00 +0300

    welcome section text updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,12 @@ import '../Styling/Home.css'
 import Container from 'react-bootstrap/Container'
 import Logo from '../Components/Logo'
 import layer6 from '../Assets/Layer6.png'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import layer11 from '../Assets/Layer11.png'
+// import layer1 from '../Assets/Layer1.png'
+
 
 
 
@@ -23,12 +29,20 @@ const Home = () => {
     </Container>
 
     {/* Welcome Section */}
-    <Container>
+    <Container fluid className='welcome-section'>
         <h2 className='welcome-text mt-4 text-center'>Welcome</h2>
-        <p className='text-center'>Dear family and friends, the time has come <br/> To <span className='spantext'> raise the roof </span>  and have some <span className='spantext'> fun </span><br/>
+        <Row>
+            <Col sm={12} md={6} lg={2}>
+                <img src={layer11} alt='flower'/>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
+            <p className='welcome-par text-center'>Dear family and friends, the time has come <br/> To <span className='spantext'> raise the roof </span>  and have some <span className='spantext'> fun </span><br/>
             Feel it in your heart and feel it in your <span className='spantext'> soul </span> <br/> Let the music <span className='spantext'>take control </span> <br/>Everybody <span className='spantext'>sing </span>, everybody <span className='spantext'>dance </span> <br/>
             Lose yourself in wild romance <br/> We’re going to <br/> <span className='spantext'> Party, Love, Fiesta, Forever </span> <br/> Join us in Los Cabos!
-        </p>
+            </p>
+            </Col>
+            <Col sm={12} md={6} lg={4}>Hello</Col>
+        </Row>
     </Container>
     
 
